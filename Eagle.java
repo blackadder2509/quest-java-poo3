9c6b3b3f462cefdb35ad69fd21d7b6b262ee277d
Anabelle Baynard <baynardanabelle@gmail.com>
2018-12-17 15:25:07 +0100

    résolution de la quete POO3

--- Eagle.java
+++ Eagle.java
@@ -1,7 +1,8 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
+
 
     public Eagle(String name) {
         super(name);
@@ -20,5 +21,30 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+    @Override
+    public void takeOff(){
+      this.flying=true;
+      System.out.println(this.getName() +" take off !");
+    }
+    @Override
+    public int ascend(int meters){
+      this.altitude += meters;
+      System.out.println(this.getName()+" flying at "+this.altitude);
+      return altitude;
+    }
+    @Override
+    public int descend(int meters){
+      this.altitude -= meters;
+      System.out.println("and descends at "+this.altitude);
+      return altitude;
+    }
+    @Override
+    public void land(){
+      if (this.altitude<=0){
+       System.out.println("he should then touch the ground");
+     }else{
+       System.out.println("he's still flying");
+     }
     }
 }
